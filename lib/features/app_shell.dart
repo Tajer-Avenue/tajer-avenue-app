af4de8c7a0655b1065ef45338feba8fee1c5e3ef
DEVWEAR <devwear9@gmail.com>
2026-09-18 05:47:17 +0400

    Open account tab after auth callback

--- lib/features/app_shell.dart
+++ lib/features/app_shell.dart
@@ -9,19 +9,32 @@ import 'pages/home_page.dart';
 import 'pages/stores_page.dart';
 
 class AppShell extends StatefulWidget {
-  const AppShell({super.key});
+  const AppShell({
+    super.key,
+    this.openAccountOnLaunch = false,
+  });
+
+  final bool openAccountOnLaunch;
 
   @override
   State<AppShell> createState() => _AppShellState();
 }
 
 class _AppShellState extends State<AppShell> {
-  int _index = 0;
+  late int _index;
   final Set<String> _favorites = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _index = widget.openAccountOnLaunch ? 4 : 0;
+  }
 
   void _toggleFavorite(ProductItem product) {
     setState(() {
-      if (!_favorites.add(product.name)) _favorites.remove(product.name);
+      if (!_favorites.add(product.name)) {
+        _favorites.remove(product.name);
+      }
     });
   }
 
@@ -29,8 +42,14 @@ class _AppShellState extends State<AppShell> {
   Widget build(BuildContext context) {
     final pages = [
       HomePage(onExplore: () => setState(() => _index = 1)),
-      ExplorePage(favorites: _favorites, onToggleFavorite: _toggleFavorite),
-      FavoritesPage(favorites: _favorites, onToggleFavorite: _toggleFavorite),
+      ExplorePage(
+        favorites: _favorites,
+        onToggleFavorite: _toggleFavorite,
+      ),
+      FavoritesPage(
+        favorites: _favorites,
+        onToggleFavorite: _toggleFavorite,
+      ),
       const StoresPage(),
       AccountPage(onOpenAuth: () => setState(() => _index = 5)),
       AuthPage(onBack: () => setState(() => _index = 4)),
@@ -38,17 +57,40 @@ class _AppShellState extends State<AppShell> {
 
     return Scaffold(
       body: IndexedStack(index: _index, children: pages),
-      bottomNavigationBar: _index == 5 ? null : NavigationBar(
-        selectedIndex: _index,
-        onDestinationSelected: (value) => setState(() => _index = value),
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.home_outlined), selectedIcon: Icon(Icons.home_rounded), label: 'Home'),
-          NavigationDestination(icon: Icon(Icons.grid_view_rounded), label: 'Explore'),
-          NavigationDestination(icon: Icon(Icons.favorite_border_rounded), selectedIcon: Icon(Icons.favorite_rounded), label: 'Saved'),
-          NavigationDestination(icon: Icon(Icons.storefront_outlined), selectedIcon: Icon(Icons.storefront_rounded), label: 'Stores'),
-          NavigationDestination(icon: Icon(Icons.person_outline_rounded), selectedIcon: Icon(Icons.person_rounded), label: 'Account'),
-        ],
-      ),
+      bottomNavigationBar: _index == 5
+          ? null
+          : NavigationBar(
+              selectedIndex: _index,
+              onDestinationSelected: (value) {
+                setState(() => _index = value);
+              },
+              destinations: const [
+                NavigationDestination(
+                  icon: Icon(Icons.home_outlined),
+                  selectedIcon: Icon(Icons.home_rounded),
+                  label: 'Home',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.grid_view_rounded),
+                  label: 'Explore',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.favorite_border_rounded),
+                  selectedIcon: Icon(Icons.favorite_rounded),
+                  label: 'Saved',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.storefront_outlined),
+                  selectedIcon: Icon(Icons.storefront_rounded),
+                  label: 'Stores',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.person_outline_rounded),
+                  selectedIcon: Icon(Icons.person_rounded),
+                  label: 'Account',
+                ),
+              ],
+            ),
     );
   }
 }
